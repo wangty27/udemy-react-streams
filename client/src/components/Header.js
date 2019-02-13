6add1beb020bd9a6bd6ef12b981f58327e094685
Tianyu Wang <terrywang@tianyu.ninja>
2019-02-12 22:25:10 -0500

    Update router & styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ import GoogleAuth from './GoogleAuth';
 
 const Header = () => {
   return (
-    <div className="ui secondary pointing menu">
+    <div className="ui secondary pointing menu" style={{padding: '5px'}}>
       <Link to="/" className="item">Streamy</Link>
       <div className="right menu">
         <Link to="/" className="item">
