b7e9ece6e5c859416533191c1d37836f03f5ec2d
Tianyu Wang <terrywang@tianyu.ninja>
2019-02-12 22:33:12 -0500

    Refactor StreamCreate to use StreamForm

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,66 +1,23 @@
 import React from 'react';
-import {Field, reduxForm} from 'redux-form';
 import {connect} from 'react-redux';
 
 import {createStream} from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamCreate extends React.Component {
-  renderError({error, touched}) {
-    if (error && touched) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      )
-    }
-  }
-
-  renderInput = formProps => {
-    const className = `field ${formProps.meta.error && formProps.meta.touched ? 'error' : ''}`;
+  render() {
     return (
-      <div className={className}>
-        <label>{formProps.label}</label>
-        <input {...formProps.input} autoComplete="off" />
-        {this.renderError(formProps.meta)}
+      <div>
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={formValues => this.props.createStream(formValues)} />
       </div>
     );
   }
-
-  onSubmit = formValues => {
-    this.props.createStream(formValues);
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
-        <Field name="title" component={this.renderInput} label="Enter Title" />
-        <Field name="description" component={this.renderInput} label="Enter Description" />
-        <button className="ui button primary">Submit</button>
-      </form>
-    );
-  }
 }
-
-const validate = formValues => {
-  var errors = {};
-
-  if (!formValues.title) {
-    errors.title = 'You must enter a title';
-  }
-  
-  if (!formValues.description) {
-    errors.description = 'You must enter a description';
-  }
-
-  return errors;
-};
 
 const mapStateToProps = () => {return {replace: 'me'}};
 
 export default connect(
   mapStateToProps,
   {createStream}
-)(reduxForm({
-  form: 'streamCreate',
-  validate
-})(StreamCreate));
+)(StreamCreate);
